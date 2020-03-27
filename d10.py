715a76b5fc27bec7422c13879d31488d7e740972
Rubayet Mahmud <m.rubayet94@gmail.com>
2020-03-27 11:25:04 +0600

    Update d10.py

--- d10.py
+++ d10.py
@@ -1,12 +1,11 @@
 num = int(input())
 count = 0
-max = 0
+maxo = 0
 while num > 0:
     if num % 2 == 1:
         count += 1
-        if count > max:
-            max = count
+        maxo = max(maxo, count)
     else:
         count = 0
     num //= 2
-print(max)
+print(maxo)
