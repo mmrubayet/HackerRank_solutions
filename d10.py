686310d29e47b83c6c18fff9bd78fd873c9c2edb
Rubayet Mahmud <m.rubayet94@gmail.com>
2020-03-27 11:09:00 +0600

    Update d10.py

--- d10.py
+++ d10.py
@@ -1,18 +1,12 @@
-def decimalToBinary(n, nl=[]):
-    if(n > 1):
-        decimalToBinary(n//2)
-    nl.append(n%2)
-    return nl
-dec = int(input())
-bin = decimalToBinary(dec)
+num = int(input())
 count = 0
 max = 0
-for num in bin:
-    if num == 1:
+while num > 0:
+    if num % 2 == 1:
         count += 1
         if count > max:
             max = count
-    if num == 0:
+    else:
         count = 0
-ans = max
-print(ans)
+    num //= 2
+print(max)
