cc1da4fa8563525e4caaae45ba95b4b81ed8c1f8
Rubayet Mahmud <m.rubayet94@gmail.com>
2020-04-17 20:50:01 +0600

    Update d29.py

--- d29.py
+++ d29.py
@@ -2,15 +2,15 @@ t = int(input())
 
 for _ in range(t):
     n, k = map(int, input().split())
-    maxab = 0
+    maximum = 0
 
     for a in range(n-1, 1, -1):
         for b in range(n, a, -1):
             ab = a&b
-            if ab < k and ab > maxab:
-                maxab = ab
-            if maxab == k-1:
+            if k > ab > maximum:
+                maximum = ab
+            if maximum == k-1:
                 break
-        if maxab == k-1:
+        if maximum == k-1:
             break
-    print(maxab)
+    print(maximum)
